100b49e85bdc4e5e0b76e25d13f1318c58fcb42f
Nimeshka Srimal <nimeshka.s@eight25media.com>
2025-02-14 17:41:47 +0530

    Add total time

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ class PomodoroTimer:
     def __init__(self, root):
         self.root = root
         self.root.title("Pomodoro Timer")
-        self.root.geometry("200x100")
+        self.root.geometry("200x150")
         self.root.attributes('-topmost', True)
         self.time_left = 12 * 60  # 12 minutes
         self.running = False
+        self.total_time = 0
         
         self.label = tk.Label(root, text="12:00", font=("Helvetica", 24))
-        self.label.pack(pady=20)
+        self.label.pack(pady=10)
+        
+        self.total_label = tk.Label(root, text="Total Time: 0:00", font=("Helvetica", 12))
+        self.total_label.pack()
         
         self.reset_button = tk.Button(root, text="Reset", command=self.reset_timer)
-        self.reset_button.pack()
+        self.reset_button.pack(pady=10)
         
         self.update_timer()
 
@@ -26,11 +30,17 @@ class PomodoroTimer:
             self.label.config(text=time_format)
             if self.time_left <= 0:
                 self.running = False
+                self.total_time += 12 * 60
+                total_mins, total_secs = divmod(self.total_time, 60)
+                self.total_label.config(text=f"Total Time: {total_mins}:{total_secs:02d}")
                 messagebox.showinfo("Time's up!", "Time for a break!")
             self.time_left -= 1
         self.root.after(1000, self.update_timer)
     
     def reset_timer(self):
+        self.total_time += (12 * 60 - self.time_left)
+        total_mins, total_secs = divmod(self.total_time, 60)
+        self.total_label.config(text=f"Total Time: {total_mins}:{total_secs:02d}")
         self.time_left = 12 * 60
         self.running = True
 
